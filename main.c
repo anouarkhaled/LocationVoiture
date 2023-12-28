96739e7c1668ed4f921ca4bc666228e34008442e
anouar.khaled <anouar.khaled2002@gmail.com>
2023-12-28 14:30:32 +0100

    ajout de fonction ecriture et lecture de fichier

--- main.c
+++ main.c
@@ -1,2 +1,40 @@
-//comment blabla
+#include <stdio.h>
+#include <stdlib.h>
+
+void EcrireFichier(){
+FILE* fichier = NULL;
+ 
+    fichier = fopen("test.txt", "w");
+ 
+    if (fichier != NULL)
+    {
+      char text [50];
+       scanf("%s",text); 
+
+        fprintf(fichier, text); // Écriture du caractère A
+        fclose(fichier);
+
+    }
+}
+
+void LireFichier(){
+   
+    FILE *f;
+    char c;
+    f=fopen("test.txt","rt");
+
+    while((c=fgetc(f))!=EOF){
+        printf("%c",c);
+    }
+
+    fclose(f);
+}
+
+
+int main() {
+   EcrireFichier();
+   LireFichier();
+    
+    return 0;
+}
 
